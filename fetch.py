eab5f6f7b03d7c97513c3f3cef83b0815b01c334
Tiago Gonzalez <tiagogonzalez2024@gmail.com>
2025-07-07 13:53:08 +0000

    some testing with fetching

--- fetch.py
+++ fetch.py
@@ -1,1 +1,17 @@
-# Start here
+import requests
+import pandas as pd
+
+url = "https://api.geode-sdk.org/v1"
+
+res = requests.get(url + "/mods")
+data = res.json()
+
+for mod in data["payload"]["data"]:
+    print({mod["id"]: {
+            "download_count": mod["download_count"], 
+            "featured": "Sí" if mod["featured"] else "No"
+        }
+    })
+
+
+
